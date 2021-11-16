980e0f690dd478d5ef28e5c4524aa87f191079a9
nahodkins <tsuber2001@gmail.com>
2021-11-16 17:58:58 +0200

    Adds 1st task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number of words: ");
+        int wordsNumber = scanner.nextInt();
+        StringBuilder builder = new StringBuilder();
+        IntStream.range(0, wordsNumber)
+                .mapToObj(i -> scanner.next())
+                .forEach(string -> {
+                    builder.append(string).append(" ");
+                    System.out.println("Current length: " + builder.length());
+                    System.out.println("Current builder content: "  + builder);
+                });
+        System.out.println("Result: " + builder);
     }
 }
